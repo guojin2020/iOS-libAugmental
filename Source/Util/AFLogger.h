3349685a366f97158a659869578d3548f18cc1b7
ie-chatton <christopherhattonuk@gmail.com>
2013-02-11 21:50:42 +0000

    Sync

--- Source/Util/AFLogger.h
+++ Source/Util/AFLogger.h
@@ -1,7 +1,8 @@
 #import <Foundation/Foundation.h>
 
 #if DEBUG
-#define AFLogPosition() NSLog(@"[%@] %@ -> %@", [[NSThread currentThread] name], NSStringFromClass([self class]), NSStringFromSelector(_cmd))
+#define AFLogMsg(M) NSLog(@"[%@] %@ -> %@ - '%@'", [NSThread currentThread], NSStringFromClass([self class]), NSStringFromSelector(_cmd), M )
+#define AFLogPosition( ) NSLog(@"[%@] %@ -> %@", [NSThread currentThread], NSStringFromClass([self class]), NSStringFromSelector(_cmd) )
 #else
 #define AFLogPosition()
 #endif
