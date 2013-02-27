078dc3d2546a031fd00cfe1679523c7fa098fbb4
ie-chatton <christopherhattonuk@gmail.com>
2013-02-27 08:39:23 +0000

    Sync

---
+++ Source/UI/View/UIView+Order.h
@@ -0,0 +1,15 @@
+//
+// Created by augmental on 27/02/2013.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+@interface UIView (Order)
+
+-(void)bringToFront;
+-(void)sendToBack;
+
+@end
