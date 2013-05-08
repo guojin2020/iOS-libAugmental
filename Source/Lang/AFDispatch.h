bf31b7450144fcd32acad7e63156dfbca7727fec
ie-chatton <christopherhattonuk@gmail.com>
2013-05-08 09:42:14 +0100

    First AppStore submission

--- Source/Lang/AFDispatch.h
+++ Source/Lang/AFDispatch.h
@@ -1,16 +1,16 @@
 
 #import <dispatch/dispatch.h>
 
-#define AFBeginMainDispatch(BLOCK)       dispatch_async(dispatch_get_main_queue(), BLOCK )
-#define AFBeginBackgroundDispatch(BLOCK) dispatch_async( AFBackgroundQueue, BLOCK )
-
-#define AFMainDispatch(BLOCK)            dispatch_sync(dispatch_get_main_queue(), BLOCK )
-#define AFBackgroundDispatch(BLOCK)      dispatch_sync( AFBackgroundQueue, BLOCK )
-
 /**
 * Globally available dispatch queue for serial background operations
 */
 extern dispatch_queue_t AFBackgroundQueue;
+
+#define AFBeginMainDispatch(BLOCK)       dispatch_async( dispatch_get_main_queue(), BLOCK )
+#define AFBeginBackgroundDispatch(BLOCK) dispatch_async( AFBackgroundQueue, BLOCK )
+
+#define AFMainDispatch(BLOCK)            dispatch_sync(dispatch_get_main_queue(), BLOCK )
+#define AFBackgroundDispatch(BLOCK)      dispatch_sync( AFBackgroundQueue, BLOCK )
 
 /**
 * Must be called, only once, before AFBeginBackgroundDispatch() is used
--- Source/Lang/AFDispatch.m
+++ Source/Lang/AFDispatch.m
@@ -1,9 +1,10 @@
 
 #import "AFDispatch.h"
 
-dispatch_queue_t AFBackgroundQueue = (dispatch_queue_t)0;
+dispatch_queue_t AFBackgroundQueue;
 
 void AFInitializeBackgroundQueue()
 {
-	AFBackgroundQueue = dispatch_queue_create("AFBackgroundQueue", DISPATCH_QUEUE_SERIAL );
+	AFBackgroundQueue = dispatch_queue_create( "AFBackgroundQueue", DISPATCH_QUEUE_SERIAL );
+	//AFBackgroundQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0 );
 }
